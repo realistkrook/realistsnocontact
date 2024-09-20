624131e5e2f2057d0ae71271e117f00ede4fb931
c9sinner <connorjsheffield@gmail.com>
2024-09-20 21:18:49 +1200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/realistsnocontact/', // Replace <repository-name> with your repository name
+  base: '/realistsnocontact/', // Replace with your repository name
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+    },
+  },
 });
